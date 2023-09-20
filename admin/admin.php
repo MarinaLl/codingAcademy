971e3054ee930c8ce39571a70c0614b58ee19565
MarinaLl <llambrichdev@gmail.com>
2023-09-20 11:35:52 +0200

    Crear profesores

---
+++ admin/admin.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Administrator Pannel</title>
+</head>
+<body>
+    <?php
+        include('funciones.php');
+        if($_POST){
+            $teacherName = $_POST['teacherName'];
+            $teacherLastNames = $_POST['teacherLastNames'];
+            $teacherTitle = $_POST['teacherTitle'];
+            $teacherEmail = $_POST['teacherEmail'];
+            $teacherPasswd = $_POST['teacherPass'];
+            $teacherPhoto = $_FILES['teacherPhoto']['tmp_name'];
+
+            $teacherPasswd = md5($teacherPasswd);
+
+            $profileImage = uploadPhoto($teacherPhoto, $_FILES['teacherPhoto']['name']);
+
+            connectDataBase();
+
+            createNewTeacher($teacherEmail, $teacherPasswd, $teacherName, $teacherLastNames, $teacherTitle, $profileImage);
+
+            showAllTeachers();
+            
+
+        } else {
+    ?>
+    <h1>Create Teacher</h1>
+    <form action="#" method="post" enctype="multipart/form-data">
+        <label for="teacherName">Name</label>
+        <input type="text" name="teacherName" id="teacherName"><br>
+        <label for="teacherLastNames">Last Names</label>
+        <input type="text" name="teacherLastNames" id="teacherLastNames"><br>
+        <label for="teacherTitle">Title</label>
+        <input type="text" name="teacherTitle" id="teacherTitle"><br>
+        <label for="teacherPhoto">Photo</label>
+        <input type="file" name="teacherPhoto" id="teacherPhoto"><br>
+        <label for="teacherEmail">Email</label>
+        <input type="email" name="teacherEmail" id="teacherEmail"><br>
+        <label for="teacherPass">Password</label>
+        <input type="password" name="teacherPass" id="teacherPass">
+        <input type="submit" value="Confirma">
+
+    </form>
+    <?php }; 
+    ?>
+</body>
+</html>
---
+++ admin/funciones.php
@@ -0,0 +1,60 @@
+<?php 
+
+function connectDataBase(){
+    $host = 'localhost';
+    $dbName = 'codingacademy';
+    $dbUsername = 'root';
+    $dbPass = '';
+
+    $connect = mysqli_connect($host, $dbUsername, $dbPass, $dbName);
+
+    if($connect == false){
+        echo mysqli_connect_error();
+    }
+
+    return $connect;
+}
+
+function uploadPhoto($profileImageTmp, $profileImageName){
+    if(is_uploaded_file($profileImageTmp)){
+        $directory = "img/";
+        $imagePath = $directory.$profileImageName;
+        move_uploaded_file($profileImageTmp, $imagePath);
+    
+    } else {
+        echo "no se ha podido subir el fichero";
+    }
+
+    return $imagePath;
+}
+
+function createNewTeacher($email, $password, $name, $lastNames, $title, $photo){
+    $sql = "INSERT INTO teacher (email, password, name, lastNames, title, photo, active) VALUES ('$email', '$password', '$name', '$lastNames', '$title', '$photo', '1')";
+    $connect = connectDataBase();
+
+    if($query = mysqli_query($connect, $sql)){
+        echo "registro insertado";
+    } else {
+        echo mysqli_errno($connect);
+    }
+}
+function showAllTeachers(){
+    $sql = "SELECT * FROM course";
+
+    $connect = connectDataBase();
+    
+    $query = mysqli_query($connect, $sql);
+
+    if ($query == false){
+        mysqli_error($connect);
+    } else {
+        $numLines = mysqli_num_rows($query);
+        for($i = 0; $i < $numLines; $i++){
+            $line = mysqli_fetch_array($query);
+            echo $line['email'];
+        }
+    }
+}
+
+
+?>
