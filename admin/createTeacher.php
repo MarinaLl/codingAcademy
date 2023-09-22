cd411ebc5c1de64c4540f694b6a36311bb7db1a9
DenisDuque <d.duque.dev@gmail.com>
2023-09-22 18:19:51 +0200

    Implementado estilo pop up add teacher

--- admin/createTeacher.php
+++ admin/createTeacher.php
@@ -37,23 +37,47 @@
     ?>
     <div class="pop-up-background">
         <div id="createTeacherPopUp">
-            <h1>Create a new teacher</h1>
+            <h1>Add new teacher</h1>
             <form action="createTeacher.php" method="post" enctype="multipart/form-data" name="createTeacher">
-                <label for="teacherName">Name</label><br>
-                <input type="text" name="teacherName" id="teacherName"><br>
-                <label for="teacherLastNames">Last Names</label><br>
-                <input type="text" name="teacherLastNames" id="teacherLastNames"><br>
-                <label for="teacherDni">DNI</label><br>
-                <input type="text" name="teacherDni" id="teacherDni"><br>
-                <label for="teacherTitle">Title</label><br>
-                <input type="text" name="teacherTitle" id="teacherTitle"><br>
-                <label for="teacherPhoto">Photo</label><br>
-                <input type="file" name="teacherPhoto" id="teacherPhoto"><br>
-                <label for="teacherEmail">Email</label><br>
-                <input type="email" name="teacherEmail" id="teacherEmail"><br>
-                <label for="teacherPass">Password</label><br>
-                <input type="password" name="teacherPass" id="teacherPass"><br>
-                <input type="submit" value="Confirma">
+                <div class="grid-container">
+                    <div class="name">
+                        <label for="teacherName">Name</label><br>
+                        <input type="text" name="teacherName" id="teacherName" class="textbox">
+                    </div>
+                    <div class="lastnames">
+                        <label for="teacherLastNames">Last names</label><br>
+                        <input type="text" name="teacherLastNames" id="teacherLastNames" class="textbox">
+                    </div>
+                    <div class="dni">
+                        <label for="teacherDni">DNI</label><br>
+                        <input type="text" name="teacherDni" id="teacherDni" class="textbox">
+                    </div>
+                
+                    <div class="title">
+                        <label for="teacherTitle">Title</label><br>
+                        <input type="text" name="teacherTitle" id="teacherTitle" class="textbox">
+                    </div>
+                    <div class="photo">
+                        <label for="teacherPhotoText">Photo</label><br>
+                        <input type="text" name="teacherPhotoText" id="teacherPhotoText" class="textbox" disabled>
+                        <label for="teacherPhoto" id="teacherPhotoBtn">Browse</label>
+                        <input type="file" name="teacherPhoto" id="teacherPhoto">
+                    </div>
+                    <div class="email">
+                        <label for="teacherEmail">Email</label><br>
+                        <input type="email" name="teacherEmail" id="teacherEmail" class="textbox">
+                    </div>
+                    <div class="password">
+                        <label for="teacherPass">Password</label><br>
+                        <input type="password" name="teacherPass" id="teacherPass" class="textbox">
+                    </div>
+                    <div class="buttons">
+                        <input id="confirmBtn" type="submit" value="Confirm">
+                        <input id="cancelBtn" type="reset" value="Cancel">  
+                    </div>
+                    
+                </div>
+                    
             </form>
         </div>
     </div>
