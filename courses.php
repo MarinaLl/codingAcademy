445ae2df479ba8f06f39aa1d502a93c53aa60ad8
DenisDuque <d.duque.dev@gmail.com>
2023-09-27 14:21:13 +0200

    Añadido Most Popular a la pagina Courses

--- courses.php
+++ courses.php
@@ -44,12 +44,56 @@
     </div>
     <h2>MOST POPULAR</h2>
     <?php
-        $sql = "SELECT TOP 3 course_code, COUNT(*) AS enrollment_count
+        $sql = "SELECT course_code, COUNT(*) AS enrollment_count
         FROM enrollment
         GROUP BY course_code
-        ORDER BY enrollment_count DESC;";
+        ORDER BY enrollment_count DESC
+        LIMIT 3";
         $connect = connectDataBase();
-        
+        $query = mysqli_query($connect, $sql);
+        if ($query == false) {
+            mysqli_error($connect);
+        } else {
+            $numLines = mysqli_num_rows($query);
+            echo $numLines;
+            if ($numLines < 3) {
+                echo '<table>';
+                for($i = 0; $i < $numLines; $i++){
+                    $line = mysqli_fetch_array($query);
+                    $sql = "SELECT * FROM course WHERE code = ".$line['course_code']."";
+                    $queryCourse = mysqli_query($connect, $sql);
+                    $course = mysqli_fetch_array($queryCourse);
+                    $sql = "SELECT name, lastNames, photo FROM teacher WHERE email = '".$course['teacher_email']."'";
+                    $queryTeacher = mysqli_query($connect, $sql);
+                    $teacher = mysqli_fetch_array($queryTeacher);
+                    $courseImage = $course['photo'];
+                    $courseName = $course['name'];
+                    $courseCode = $line['course_code'];
+                    $teacherPhoto = $teacher['photo'];
+                    $teacherCompleteName = $teacher['name']." ".$teacher['lastNames'];
+                    $courseDescription = $course['description'];
+                    $courseDuration = $course['duration'];
+                    $courseStartDate = $course['start'];
+                    $courseDifficulty = $course['difficulty'];
+                    
+                    echo '
+                        <tr>
+                            <td><img src="'.$courseImage.'"></td>
+                            <td>'.$courseName.'</td>
+                            <td><button type="submit" name="buttonEnroll" value='.$courseCode.'>Enroll</button></td>
+                            <td><img src="'.$teacherPhoto.'"></td>
+                            <td>'.$teacherCompleteName.'</td>
+                            <td>'.$courseDescription.'</td>
+                            <td>'.$courseDuration.'</td>
+                            <td>'.$courseStartDate.'</td>
+                            <td>'.$courseDifficulty.'</td>
+                        </tr>';
+                }
+                echo '</table></form>';
+            } else {
+                echo 'No hay cursos en esta categoria';
+            }
+        }
     ?>
 </body>
 </html>
