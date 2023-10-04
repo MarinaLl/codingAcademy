8a3da75fe4ab317afc1a58409703de9b0d3ef3e8
MarinaLl <llambrichdev@gmail.com>
2023-10-04 14:27:11 +0200

    creado autogenerador de gridareas

--- student/student.php
+++ student/student.php
@@ -25,54 +25,69 @@
             }
         }
     ?>
-    <h1>Hello, <?php echo $_SESSION['completeName']; ?></h1>
-    <a id="editProfileButton" href="editProfile.php">Edit Profile</a>
-    <form action="student.php" method="post" name="unenrollCourse">
-    <?php 
-        $sql = "SELECT  t.photo AS teacherPhoto, 
-                        t.name AS teacherName,
-                        t.lastNames AS teacherLastNames,
-                        c.code AS courseCode,
-                        c.photo AS coursePhoto, 
-                        c.name AS courseName, 
-                        c.start AS courseStart, 
-                        c.end AS courseEnd,
-                        e.grade AS grade 
-                FROM ((course c INNER JOIN teacher t ON c.teacher_email = t.email) 
-                INNER JOIN enrollment e ON e.course_code = c.code)
-                WHERE e.student_email = '".$_SESSION['user']."'";
-        $connect = connectDataBase();
-        
-        $query = mysqli_query($connect, $sql);
+    <div class="grid-container">
+        <div></div>
+        <div id="studentPanel">
+            <div>
+                <h1>Hello, <?php echo $_SESSION['completeName']; ?></h1>
+                <a id="editProfileButton" href="editProfile.php">Edit Profile</a>
+            </div>
+            <form action="student.php" method="post" name="unenrollCourse">
+            <?php 
+                $sql = "SELECT  t.photo AS teacherPhoto, 
+                                t.name AS teacherName,
+                                t.lastNames AS teacherLastNames,
+                                c.code AS courseCode,
+                                c.photo AS coursePhoto, 
+                                c.name AS courseName, 
+                                c.start AS courseStart, 
+                                c.end AS courseEnd,
+                                e.grade AS grade 
+                        FROM ((course c INNER JOIN teacher t ON c.teacher_email = t.email) 
+                        INNER JOIN enrollment e ON e.course_code = c.code)
+                        WHERE e.student_email = '".$_SESSION['user']."'";
+                $connect = connectDataBase();
+                
+                $query = mysqli_query($connect, $sql);
 
-        if ($query == false){
-            mysqli_error($connect);
-        } else {
-            $numLines = mysqli_num_rows($query);
-            
-            if ($numLines > 0) {
-                echo '<table>';
-                for($i = 0; $i < $numLines; $i++){
-                    $line = mysqli_fetch_array($query);
+                if ($query == false){
+                    mysqli_error($connect);
+                } else {
+                    $numLines = mysqli_num_rows($query);
                     
-                    echo '
-                        <tr>
-                            <td><img src="'.$line['coursePhoto'].'"></td>
-                            <td>'.$line['courseName'].'</td>
-                            <td><img src="'.$line['teacherPhoto'].'"></td>
-                            <td>'.$line['teacherName'].' '.$line['teacherLastNames'].'</td>
-                            <td>'.$line['courseStart'].'</td>
-                            <td>'.$line['courseEnd'].'</td>
-                            <td><button type="submit" name="buttonUnenroll" value='.$line['courseCode'].'>Unenroll</button></td>
-                            <td>Grade: '.$line['grade'].' / 10</td>
-                        </tr>';
+                    if ($numLines > 0) {
+                        echo '<table>';
+                        for($i = 0; $i < $numLines; $i++){
+                            $line = mysqli_fetch_array($query);
+                            
+                            echo '
+                                <tr>
+                                    <td><img src="'.$line['coursePhoto'].'"></td>
+                                    <td>'.$line['courseName'].'</td>
+                                    <td><img src="'.$line['teacherPhoto'].'"></td>
+                                    <td>'.$line['teacherName'].' '.$line['teacherLastNames'].'</td>
+                                    <td>'.$line['courseStart'].'</td>
+                                    <td>'.$line['courseEnd'].'</td>
+                                    <td><button type="submit" name="buttonUnenroll" value='.$line['courseCode'].'>Unenroll</button></td>
+                                    <td>Grade: '.$line['grade'].' / 10</td>
+                                </tr>';
+                        }
+                        echo '</table></form>';
+                    } else {
+                        echo 'You are not enrolled in any course.';
+                    }
                 }
-                echo '</table></form>';
-            } else {
-                echo 'You are not enrolled in any course.';
-            }
-        }
-    ?>
+            ?>
+            <div class="cardComponent">
+                <div></div>
+                <div>
+                    <h4>javascript basics</h4>
+                    <h5>joel gurrera</h5>
+                </div>
+            </div>
+        </div>
+        <div></div>
+    </div>
 </body>
 </html>
 <?php } else {
