5049613a99a5ab9820b00282772f1cfd2902be8e
DenisDuque <d.duque.dev@gmail.com>
2023-10-24 12:35:20 +0200

    testimonials

--- footer.php
+++ footer.php
@@ -11,22 +11,26 @@
     <?php echo '<link rel="stylesheet" href="'.$path.'css/main.css">'?>
 </head>
 <body>
-    <footer>
+    <footer class="grid-container">
+        <div></div>
         <?php 
             echo '
             <div>
-                <div id="links">
+                <div>
                     <a href="'.$path.'index.php">Home</a>
                     <a href="'.$path.'student/courses.php">Courses</a>
                     <a href="'.$path.'aboutUs.php">About Us</a>
                     <a href="'.$path.'contact.php">Contact</a>
+                </div>
+                <div>
                     <a href="https://www.youtube.com/"><img id="logo" src="'.$path.'src/youtube.png" alt="youtube"></a>
                     <a href="https://www.facebook.com/"><img id="logo" src="'.$path.'src/facebook.png" alt="facebook"></a>
                     <a href="https://www.instagram.com/"><img id="logo" src="'.$path.'src/instagram.png" alt="instagram"></a>
                     <a href="http://x.com/"><img id="logo" src="'.$path.'src/twitter.png" alt="twitter"></a>
-                </div>
+                </div> 
             </div>';
             ?>
+        <div></div>
     </footer>
 </body>
 </html>
--- index.php
+++ index.php
@@ -34,14 +34,36 @@
     <div class="grid-container">
         <div></div>
         <div>
-
+            <div id="testimonials">
+                    <h4>TESTIMONIALS</h4>
+                    <h5>STUDENTS REVIEWS</h5>
+                    <div>
+                        <div>
+                            <div><img src="src/Ellipse.png"></div>
+                            <div><h6>Iker Gonzalez</h6></div>
+                            <div><p>"The programming courses on this online platform are absolutely exceptional! Since I enrolled, my professional life has undergone an incredible transformation. The instructors are extremely knowledgeable and passionate about their subjects, which is reflected in the quality of the lessons."</p></div>
+                        </div>
+                        <div>
+                            <div><img src="src/Ellipse.png"></div>
+                            <div><h6>Iker Gonzalez</h6></div>
+                            <div><p>"The lessons are logically structured, making learning a breeze, especially for someone like me with no prior programming experience. Moreover, the platform is very user-friendly and easy to navigate, making finding and accessing course materials a straightforward task."</p></div>
+                        </div>
+                        <div>
+                            <div><img src="src/Ellipse.png"></div>
+                            <div><h6>Iker Gonzalez</h6></div>
+                            <div><p>"The course content is comprehensive and up-to-date, allowing me to learn the latest technologies and programming languages. I also appreciate the flexibility of being able to study at my own pace, which has been crucial in balancing my studies with my full-time job."</p></div>
+                        </div>
+                        <div>
+                            <div><img src="src/Ellipse.png"></div>
+                            <div><h6>Iker Gonzalez</h6></div>
+                            <div><p>"In summary, if you're looking for an online learning platform to study programming, look no further! This platform is the perfect choice. The results speak for themselves, and I'm thrilled with everything I've learned so far. I can't wait to continue advancing in my career thanks to these exceptional courses!"</p></div>
+                        </div>
+                    </div>
+                </div>
         </div>
         <div></div>
     </div>
-    <div id="testimonials">
-        <h4>TESTIMONIALS</h4>
-
-    </div>
+    
     
     <script src="main.js"></script>
 
